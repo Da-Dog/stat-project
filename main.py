0976128179322b5fc545711d8b2d53583fd8db03
Da_Dog <jiawenli233@gmail.com>
2025-04-25 22:55:44 -0700

    removed main.py, completed scheduler.py

--- main.py
+++
@@ -1,7 +0,0 @@
-"""
-RUN scheduler.py INSTEAD
-main() will be called by scheduler.py every day at 8 AM and 8 PM
-"""
-
-def main():
-    print("Running main function...")
--- scheduler.py
+++ scheduler.py
@@ -1,16 +1,26 @@
-from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.schedulers.blocking import BlockingScheduler
 from datetime import datetime
-import main
+from data import get_data, print_availability_report, log_data
 
 
 def job():
     try:
-        main.main()
+        spaces_names, slot_counter = get_data()
+        print_availability_report(spaces_names, slot_counter)
+        log_data(spaces_names, slot_counter)
     except Exception as e:
         print(f"Job failed at {datetime.now()}: {e}")
 
 
+def alive_check():
+    print(f"Running... Time is: {datetime.now().isoformat()}")
+
+
 if __name__ == '__main__':
-    scheduler = BackgroundScheduler()
+    scheduler = BlockingScheduler()
     scheduler.add_job(job, 'cron', hour='8,20', minute=0)
+    scheduler.add_job(alive_check, 'interval', minutes=30)  # Run every hour
+
+    print("Scheduler starting. Press Ctrl+C to exit.")
+
     scheduler.start()
